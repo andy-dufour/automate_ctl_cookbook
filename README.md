71327ca3aa177eaca7001372898158dd34280ad3
John Snow <thelunaticscripter@outlook.com>
2017-06-21 13:56:51 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# automate_ctl_cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: automate_ctl_cookbook
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
